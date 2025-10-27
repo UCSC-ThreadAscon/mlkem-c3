9c899b9e9e31e030ed558bd1ad2110605626caf5
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-10-27 14:08:33 -0700

    Added ML-KEM native source code

--- main/main.c
+++ main/main.c
@@ -15,15 +15,7 @@ void app_main(void)
 {
 #if (CONFIG_MLKEM == 0)
   hello_world();
-#elif (CONFIG_MLKEM == 2)
-  unsigned char pk[CRYPTO_PUBLICKEYBYTES];
-  unsigned char sk[CRYPTO_SECRETKEYBYTES];
-
-  esp_fill_random(pk, sizeof(pk));
-  esp_fill_random(sk, sizeof(sk));
-
-  crypto_kem_keypair(pk, sk);
-#elif (CONFIG_MLKEM == 4)
+#else
   unsigned char pk[CRYPTO_PUBLICKEYBYTES];
   unsigned char sk[CRYPTO_SECRETKEYBYTES];
 
